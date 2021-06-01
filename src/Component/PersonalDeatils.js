dc975995e203a7530f5ab5818a1eed6bc8dc24dd
Subhadeep <subhadeepmodak00@gmail.com>
2021-06-01 14:10:21 +0530

    Final_Commit( UI)

--- src/Component/PersonalDeatils.js
+++ src/Component/PersonalDeatils.js
@@ -22,9 +22,9 @@ function Personal() {
     
 
     <div className={classes.root} >
-      <TextField id="outlined-basic"  value={Data['name']} onChange={(e)=>setData({...Data,'name':e.target.value})} label="Name" variant="outlined" error={Data['nameError']} />
-      <TextField id="outlined-basic"  value={Data['age']} onChange={(e)=>setData({...Data,'age':e.target.value})}  label="Age" variant="outlined" error={Data['ageError']}/>
-      <TextField id="outlined-basic"  value={Data['mobile']} onChange={(e)=>setData({...Data,'mobile':e.target.value})} label="Mobile" variant="outlined" error={Data['mobileError']}/>
+      <TextField id="outlined-basic"  value={Data['name']} onChange={(e)=>setData({...Data,'name':e.target.value})} label="Name" variant="outlined" error={Data['nameError']} helperText={Data['nameError']?"Something Wrong, plese fill correctly!!":""}/>
+      <TextField id="outlined-basic"  value={Data['age']} onChange={(e)=>setData({...Data,'age':e.target.value})}  label="Age" variant="outlined" error={Data['ageError']} helperText={Data['ageError']?"Something Wrong, plese fill correctly!!":""}/>
+      <TextField id="outlined-basic"  value={Data['mobile']} onChange={(e)=>setData({...Data,'mobile':e.target.value})} label="Mobile" variant="outlined" error={Data['mobileError']} helperText={Data['mobileError']?"Something Wrong, plese fill correctly!!":""}/>
     </div>
     </>
   );
